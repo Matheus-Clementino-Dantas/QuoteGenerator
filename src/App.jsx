cebe5172843acbc53fa601458f3d42bf57ffc905
Matheus Clementino Dantas <matheusclementino27@gmail.com>
2026-01-12 21:07:01 -0300

    style: add @theme with color palette, shadows, and base fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
 function App() {
-  return <h1>Hello</h1>;
+  return (
+    <div className="bg-bg-dark min-h-screen flex items-center justify-center">
+      <div className="bg-bg px-8 py-6 rounded-lg shadow-md">
+        <h1 className="text-text-light text-2xl font-bold font-site">Teste</h1>
+        <p className="text-text-dark text-lg font-site">
+          Apenas testando as cores e sombras etc.
+        </p>
+        <button className="text-text-light bg-bg-light p-2 font-site rounded-md mt-4 font-medium text-xl">
+          Clica ai
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
