92492362551a6d53f7acbab94ac3d294af8f5ef6
Matheus Clementino Dantas <matheusclementino27@gmail.com>
2026-01-16 19:57:31 -0300

    feat: add QuoteCard component

---
+++ src/components/QuoteCard.jsx
@@ -0,0 +1,20 @@
+function QuoteCard({ isloading, className, quote, children }) {
+  return (
+    <article
+      className={`shadow-sm bg-bg rounded-lg py-4 px-6 ${
+        isloading ? "animate-pulse" : ""
+      } ${className}`}
+    >
+      <figure className="space-y-1 sm:space-y-2.5">
+        <blockquote className="text-text-light font-quote text-lg md:text-2xl lg:text-3xl">
+          "{quote?.quote}"
+        </blockquote>
+        <figcaption className="text-end font-semibold font-site text-text-dark text-sm md:text-xl lg:text-2xl">
+          - {quote?.author}
+        </figcaption>
+      </figure>
+      {children}
+    </article>
+  );
+}
+export default QuoteCard;
