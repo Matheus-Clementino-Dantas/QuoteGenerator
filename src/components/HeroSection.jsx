22d4a88a91d5445818549b57658438bfda43ff85
Matheus Clementino Dantas <matheusclementino27@gmail.com>
2026-01-19 21:10:34 -0300

    feat: create HeroSection component

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,7 +2,7 @@ function HeroSection() {
   return (
     <header className="h-screen flex items-center justify-center w-screen">
       <section className="space-y-2 flex flex-col items-center justify-center">
-        <h1 className="text-text-light text-5xl font-site sm:text-6xl md:text-8xl tracking-tighter font-semibold">
+        <h1 className="text-text-light text-5xl font-site sm:text-6xl md:text-8xl lg:text-9xl tracking-tighter font-semibold">
           “All roads lead to <br />
           QuoteGenerator.”
         </h1>
@@ -12,13 +12,13 @@ function HeroSection() {
         <div className="flex items-center justify-center gap-2 w-full">
           <a
             href="#RandomQuote"
-            className="p-3 bg-bg-light/90 text-white rounded-md font-semibold font-site shadow-sm text-xl leading-none"
+            className="p-2.5 bg-bg-light/90 text-white rounded-md font-semibold font-site shadow-sm text-lg transition-transform duration-300 leading-none sm:text-xl sm:p-3 hover:scale-95"
           >
             Random Quote
           </a>
           <a
             href="#Explore"
-            className="p-3 bg-white/80 text-black/90 rounded-md font-semibold font-site shadow-sm text-xl leading-none"
+            className="p-2.5 bg-white/80 text-black/90 rounded-md font-semibold font-site shadow-sm text-lg leading-none sm:text-xl sm:p-3 transition-transform duration-300 hover:scale-95"
           >
             Explore Quotes
           </a>
